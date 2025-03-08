cdea068e59cd98ecefaf3daa6075794dc4967c23
ak4599525 <147978055+ak4599525@users.noreply.github.com>
2025-03-08 21:00:42 +0530

    Initial commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -31,14 +31,13 @@ const Footer = () => {
         <div className="footer-content-right">
           <h2>GET IN TOUCH</h2>
           <ul>
-            <li>+91 9894019281</li>
-            <li>arunmuthaiah007@gmail.com</li>
+            <li>ak4599525@gmail.com</li>
           </ul>
         </div>
       </div>
       <hr></hr>
       <p className="footer-copyright">
-        Copyright 2024 © Arunkumar -All Rights Reserved
+        Copyright 2024 © Akashdeep -All Rights Reserved
       </p>
     </div>
   );
